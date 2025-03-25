c95e6a10cc524732ff211760d4ef5212ef834001
SariaAskort <sariaaskort@tuta.io>
2025-03-25 22:29:49 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BookCounter',
+  webDir: 'www'
+};
+
+export default config;
